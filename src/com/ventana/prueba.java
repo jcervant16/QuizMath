a95bf2154774a92320d3fbf9e229ea8aa36064ed
Jose Cervantes <jose11cervantes@hotmail.com>
2018-10-25 15:39:15 -0500

    ya funciona el sistema de puntos y vidas

---
+++ src/com/ventana/prueba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ventana;
+
+import com.metodos.metodos;
+
+/**
+ *
+ * @author jose1
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        metodos m = new metodos();
+        System.out.println(m.signoRandom());
+    }
+    
+}
